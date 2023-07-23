57895f94e98b1583605f78c142f344221e106a0f
TRAN CANH THANH <trancanhthanh1@gmail.com>
2023-07-23 03:20:54 -0400

    Practice with redux

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 
 // Add code to import the other components here under
-
+import Remaining from './components/Remaining';
 
 import { AppProvider } from './context/AppContext';
 const App = () => {
@@ -16,9 +16,9 @@ const App = () => {
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
                         {/* Add Budget component here under */}        
-
+                    <div className='col-sm'><Budget/></div>
                         {/* Add Remaining component here under */}        
-
+                    <div className='col-sm'><Remaining/></div>
                         {/* Add ExpenseTotal component here under */}        
                        
                         {/* Add ExpenseList component here under */}         
